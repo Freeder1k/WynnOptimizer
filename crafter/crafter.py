2d528788f1112dd22b5df1bfecd13107979c5d84
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-04-17 23:00:23 +0200

    use multiple processes

--- crafter/crafter.py
+++ crafter/crafter.py
@@ -1,17 +1,35 @@
+from multiprocessing import Pool
+
 from core.optimizer import bruteForce
 from . import ingredient, recipe
 
 
 async def optimize(stat: str, min_dura: int, ingredients: list[ingredient.Ingredient]):
-    best = ingredient.NO_INGREDIENT
-    best_r = None
-    best.identifications[stat] = ingredient.Identification(0)  # uhh yeah... ig it works
+    with Pool(6) as p:
+        results = p.starmap(_get_best_recipe, [(stat, min_dura, i, ingredients) for i in ingredients])
 
-    for combination in bruteForce.generate_all_combinations(6, *ingredients):
-        r = recipe.Recipe(*combination)
+    best_r = results[0]
+    for r in results:
+        if r.build().identifications[stat].max > best_r.build().identifications[stat].max:
+            best_r = r
+
+    return best_r
+
+
+def _get_best_recipe(stat: str, min_dura: int, first_ing, ingredients):
+    best = ingredient.Ingredient("base", 0, 0, 0, {'stat': ingredient.Identification(0)}, ingredient.Modifier(),
+                                 ingredient.Requirements(set()))
+    best_r = None
+    best.identifications[stat] = ingredient.Identification(0)
+
+    for combination in bruteForce.generate_all_combinations(5, *ingredients):
+        r = recipe.Recipe(first_ing, *combination)
         item = r.build()
 
-        if item.identifications[stat].max > best.identifications[stat].max and item.durability >= min_dura:
+        if not stat in item.identifications or item.durability < min_dura:
+            continue
+
+        if item.identifications[stat].max > best.identifications[stat].max:
             best = item
             best_r = r
 
