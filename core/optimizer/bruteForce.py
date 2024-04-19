23a42b4264e48c4b31268d2548687f284a5e07dd
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-04-19 15:55:57 +0200

    improve brute force functions

--- core/optimizer/bruteForce.py
+++ core/optimizer/bruteForce.py
@@ -1,12 +1,24 @@
-from typing import TypeVar, Generator, Tuple
+from typing import TypeVar, Generator
 
 T = TypeVar('T')
 
-def generate_all_combinations(n: int, *choices: T) -> Generator[Tuple[T], None, None]:
+
+def generate_all_permutations(n: int, *choices: T, repeat=False, ordered=False) -> Generator[tuple[T], None, None]:
     if n == 1:
         for choice in choices:
             yield (choice,)
     else:
-        for sub_combination in generate_all_combinations(n - 1, *choices):
-            for choice in choices:
-                yield (choice,) + sub_combination
+        if len(choices) == 0:
+            return  # Don't yield since the result would be shorter than the requested n
+        for i in range(len(choices)):
+            for sub_choice in generate_all_permutations(n - 1, *(choices[:i] if not ordered else ()),
+                                                        *choices[i if repeat else i + 1:], repeat=repeat,
+                                                        ordered=ordered):
+                yield (choices[i],) + sub_choice
+
+
+def generate_all_subpermutations(*choices: T, repeat: bool = False, ordered: bool = False) -> Generator[
+    tuple[T], None, None]:
+    for i in range(1, len(choices) + 1):
+        for choice in generate_all_permutations(i, *choices, repeat=repeat, ordered=ordered):
+            yield choice
