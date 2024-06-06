6b857eed8a5a3fec45ca328348787487a881762a
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-06-06 15:00:35 +0200

    merge

--- mainbuild.py
+++ mainbuild.py
@@ -2,7 +2,9 @@ import build.fastHybridOptimizer
 import build.item
 import build.build
 from build.config.dmg import DmgConfig
-
+import build.ortoolssolver
+import faulthandler
+faulthandler.enable()
 
 def main():
     print(f" ∧,,∧\n"
@@ -11,9 +13,18 @@ def main():
           "し———J\n")
 
     cfg = DmgConfig()
+    solver = build.ortoolssolver.CPModelSolver(cfg.items, cfg.score_function, cfg.weapon)
+    results = solver.find_allbest()
+
+    with open('array.txt', 'w') as f:
+        for entry in results:
+            f.write(f"{entry.items}\n")
+
+
+    '''cfg = DmgConfig()
     res = build.fastHybridOptimizer.optimize(cfg)
     if res is not None:
-        print_build(res)
+        print_build(res)'''
 
 
 def print_build(b: build.build.Build):
