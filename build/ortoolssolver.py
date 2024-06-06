86de5d3f6b548ec2ba34e8322618b0bf640e2c97
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-06-06 17:58:30 +0200

    improve skillpoint constraints

--- build/ortoolssolver.py
+++ build/ortoolssolver.py
@@ -75,15 +75,14 @@ class CPModelSolver:
             for itm, x in zip(t_items, t_vars):
                 for sp_req, itm_sp_bonus, itm_sp_req in zip(sp_reqs, itm.identifications.skillpoints, itm.requirements.skillpoints):
                     if itm_sp_req != 0:
-                        sp_req.append((itm_sp_bonus + itm_sp_req) * x)
-                    else:
-                        sp_req.append(-1000 * x)
+                        sp_req.append((itm_sp_bonus + itm_sp_req + 1000) * x)
 
             for sp_assign, sp_req, sp_bonus in zip(self.sp_assignment_vars, sp_reqs, sp_bonuses):
-                self.model.add(sp_assign >= sum(sp_req) - sum(sp_bonus))
+                self.model.add(sp_assign >= sum(sp_req) - 1000 - sum(sp_bonus))
 
         for sp_assign, sp_req, sp_bonus in zip(self.sp_assignment_vars, weapon.requirements.skillpoints, sp_bonuses):
-            self.model.add(sp_assign >= sp_req - sum(sp_bonus))
+            if sp_req != 0:
+                self.model.add(sp_assign >= sp_req - sum(sp_bonus))
 
         self._objective = [score_function(itm) * x for itm, x in zip(self._items, self.item_variables)]
         self.model.add(sum(self._objective) > 7000)
