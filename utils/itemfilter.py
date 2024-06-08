2e56ca79accd37edb7f8a0f6788d020fbdb6e3c3
afterfive <119972495+afterfive7@users.noreply.github.com>
2024-06-08 16:29:58 +0200

    tweaked rings in itemfilter

--- utils/itemfilter.py
+++ utils/itemfilter.py
@@ -2,7 +2,7 @@ import build.item
 
 types = ['helmet', 'chestplate', 'leggings', 'boots', 'ring', 'bracelet', 'necklace']
 damageTypes = ["damage", "earthDamage", "thunderDamage",  "waterDamage", "fireDamage", "airDamage"]
-
+set_rings = ['Intensity', 'Breezehands', 'Coral Ring', 'Moon Pool Circlet']
 
 def remove_bad_items(base_dmg, items: list[build.item.Item]) -> list[build.item.Item]:
     relevant_ids = ["rawStrength", "rawDexterity", "rawIntelligence", "rawDefense", "rawAgility", 'rawSpellDamage', 'spellDamage']
@@ -14,17 +14,13 @@ def remove_bad_items(base_dmg, items: list[build.item.Item]) -> list[build.item.
         for itm in t_items:
             good = True
             for itm2 in t_items:
-                if all(itm.requirements >= itm2.requirements):
+                if all(itm.requirements >= itm2.requirements) and itm2.name not in set_rings:
                     if all(itm2.identifications[id].max >= itm.identifications[id].max for id in relevant_ids):
                         if any(itm2.identifications[id].max > itm.identifications[id].max for id in relevant_ids):
                             good = False
-                            #print(itm.name, itm2.name)
                             break
             if good:
                 good_items.append(itm)
-
-    for t in types:
-        t_items = [i for i in good_items if i.type == t]
 
     return good_items
 
