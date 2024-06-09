a281106bd19170c47017963512dbd3371b1b9769
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-06-10 00:04:26 +0200

    add ingreds_clean.json

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ from craft.config.base import HybridOptimizerConfig
 def score(ingr: craft.ingredient.Ingredient) -> float:
     return (
             100 * ingr.identifications['spellDamage'].max
-            + 56 * ingr.identifications['thunderDamage'].max
-            + 35 * ingr.identifications['airDamage'].max
+            + 30 * ingr.identifications['manaRegen'].max
+            + 12 * ingr.identifications['rawSpellDamage'].max
+            + 8 * ingr.identifications['earthDamage'].max
+            + 4 * ingr.identifications['thunderDamage'].max
+            + 4 * ingr.identifications['waterDamage'].max
+            + 17 * ingr.identifications['fireDamage'].max
+            + 64 * ingr.identifications['airDamage'].max
             + ingr.durability / 100000)
 
 
@@ -24,22 +29,22 @@ def main():
 
     cfg = HybridOptimizerConfig(
         ingredients=list(i for i in craft.ingredient.get_all_ingredients().values()
-                         if i.modifiers.abs_total() == 0 and 'jeweling' in i.requirements.skills),
+                         if i.modifiers.abs_total() == 0 and 'tailoring' in i.requirements.skills),
         score_function=score,
-        crafting_skill='jeweling',
-        relevant_ids=[craft.ingredient.IdentificationType.SPELL_DAMAGE,
-                      craft.ingredient.IdentificationType.THUNDER_DAMAGE,
-                      craft.ingredient.IdentificationType.AIR_DAMAGE]
+        crafting_skill='tailoring',
+        relevant_ids=[craft.ingredient.IdentificationType.MANA_REGEN,
+                      craft.ingredient.IdentificationType.SPELL_DAMAGE]
     )
 
-    cfg.set_min_durability(30)
-    cfg.set_max_str_req(100)
-    cfg.set_max_dex_req(120)
-    cfg.set_max_int_req(65)
+    cfg.set_min_durability(90)
+    cfg.set_max_str_req(25)
+    cfg.set_max_dex_req(23)
+    cfg.set_max_int_req(77)
     cfg.set_max_def_req(0)
-    cfg.set_max_agi_req(0)
-    cfg.set_identification_min(craft.ingredient.IdentificationType.MANA_REGEN, 0)
+    cfg.set_max_agi_req(110)
+    cfg.set_identification_min(craft.ingredient.IdentificationType.MANA_REGEN, 13)
     cfg.set_identification_min(craft.ingredient.IdentificationType.RAW_INTELLIGENCE, 0)
+    cfg.set_identification_min(craft.ingredient.IdentificationType.RAW_STRENGTH, 0)
 
     res = craft.fastHybridOptimizer.optimize(cfg)
     print_recipe(res)
@@ -47,10 +52,11 @@ def main():
 
 def print_recipe(r: craft.recipe.Recipe) -> str:
     item = r.build()
-    print(f"https://hppeng-wynn.github.io/crafter/#1{r.b64_hash()}9m91 "
+    print(f"https://hppeng-wynn.github.io/crafter/#1{r.b64_hash()}9i91 "
             f"{item.identifications['spellDamage'].max} sd "
-            f"{item.identifications['thunderDamage'].max} td "
+            f"{item.identifications['rawSpellDamage'].max} raw sd "
             f"{item.identifications['airDamage'].max} ad "
+            f"{item.identifications['fireDamage'].max} fd "
             # f"{eff_defagi(item)} def+agi "
             f"{(item.durability + 735000) // 1000} dura   "
             # f"{r}"
