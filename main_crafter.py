9c9fc2496bafb842dc667311123db9866ccf83c9
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-07-13 15:55:57 +0200

    add main_crafter.py

---
+++ main_crafter.py
@@ -0,0 +1,57 @@
+import craft.optimizerCP
+import wynndata.ingredient
+
+
+def main():
+    ingredients = list(i for i in wynndata.ingredient.get_all_ingredients().values()
+                       if wynndata.ingredient.Profession('armouring') in i.skills
+                       and (i.durability > 0
+                            or i.identifications.healingEfficiency.abs_max != 0
+                            # or i.identifications.manaRegen.abs_max != 0
+                            or i.identifications.rawHealth.abs_max != 0
+                            or i.identifications.waterDamage.abs_max != 0
+                            # or i.identifications.thunderDamage.abs_max != 0
+                            # or i.identifications.spellDamage.abs_max != 0
+                            # or i.identifications.lootBonus.abs_max != 0
+                            # or i.identifications.lootQuality.abs_max != 0
+                            # or i.requirements.sp_total != 0
+                            or i.modifiers.abs_total() != 0))
+    # ingredients = [i for i in ingredients if i.name != "Elephelk Trunk"]
+    print(len(ingredients))
+    solver = craft.optimizerCP.CPRecipeOptimizer(
+        ingredients=ingredients,
+    )
+
+    recipe = solver.recipe
+
+    water_dmg_var = solver.model.new_int_var(0, 5000, "water_dmg")
+    health_var = solver.model.new_int_var(20000, 40000, "health")
+    healing_eff_var = solver.model.new_int_var(0, 500, "healing_eff")
+
+    solver.model.add(water_dmg_var == 3 * recipe.identifications.waterDamage.abs_max + 1000 + 93)
+    solver.model.add(health_var == recipe.identifications.rawHealth.abs_max + 27408)
+    solver.model.add(healing_eff_var == recipe.identifications.healingEfficiency.abs_max + 100 + 160)
+
+    water_x_hp = solver.model.new_int_var(0, 160000000, "water_x_hp")
+    solver.model.add_multiplication_equality(water_x_hp, water_dmg_var, health_var)
+
+    healing = solver.model.new_int_var(0, 80000000000, "healing")
+    solver.model.add_multiplication_equality(healing, water_x_hp, healing_eff_var)
+
+    solver.set_objective(((0
+                           + healing * 0.0000025
+                           ) * 100
+                          + recipe.durability))
+
+    solver.add(recipe.durability >= 30)
+    solver.add(recipe.requirements.strength <= 0)
+    solver.add(recipe.requirements.dexterity <= 0)
+    solver.add(recipe.requirements.intelligence <= 120)
+    solver.add(recipe.requirements.defence <= 115)
+    solver.add(recipe.requirements.agility <= 0)
+
+    print(solver.find_best())
+
+
+if __name__ == '__main__':
+    main()
