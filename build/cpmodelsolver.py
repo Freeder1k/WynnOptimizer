3a41ad0d3b262a4c61d02a9d6de01c09cac797b7
afterfive <119972495+afterfive7@users.noreply.github.com>
2024-07-15 01:54:49 +0200

    tweaked dmg model (somehow doesnt work even though only factors are different)

--- build/cpmodelsolver.py
+++ build/cpmodelsolver.py
@@ -87,7 +87,7 @@ class CPModelSolver:
             self.model.add_max_equality(sp_assign + sum(sp_bonus), sp_req)
 
         # Set the objective function
-        self.damage, self.testvars = dmgcalc.true_dmg_model2(self.model, dmg.base_dmg_min, dmg.base_dmg_max, self._items, self.item_variables, self.sp_assignment_vars, self._weapon, dmg.spellmod, [False] + dmg.mastery)
+        self.damage, self.testvars = dmgcalc.true_dmg_model(self.model, dmg.base_dmg_min, dmg.base_dmg_max, self._items, self.item_variables, self.sp_assignment_vars, self._weapon, dmg.spellmod, [False] + dmg.mastery)
 
         #self._objective = [int(score_function(itm)) * x for itm, x in zip(self._items, self.item_variables)]
 
@@ -158,7 +158,7 @@ class CPModelSolver:
 
     def _find(self, silent=False):
         solver = cp_model.CpSolver()
-        solution_printer = VarArraySolutionPrinter(self.item_variables, self._items, self._weapon, [sum(self.damage)] + self.testvars, silent)
+        solution_printer = VarArraySolutionPrinter(self.item_variables, self._items, self._weapon, [self.damage] + self.testvars, silent)
         solver.parameters.enumerate_all_solutions = True
         status = solver.solve(self.model, solution_printer)
         if not silent:
@@ -169,9 +169,9 @@ class CPModelSolver:
         return solution_printer.solution_count
 
     def find_best_new(self):
-        #self.model.maximize(sum(self.damage))
+        #self.model.maximize(self.damage)
         self.model.clear_objective()
-        self.model.add(self.damage >= 500000000)
+        self.model.add(self.damage >= 0)
         return self._find()
 
     def find_best(self, factor):
